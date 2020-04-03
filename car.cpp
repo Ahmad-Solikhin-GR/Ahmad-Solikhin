bbeb658a3c8cb4839af70f7233f09e2459a3e053
Ahmad-Solikhin-GR <63105261+Ahmad-Solikhin-GR@users.noreply.github.com>
2020-04-03 19:36:09 +0700

    Create car.cpp

---
+++ car.cpp
@@ -0,0 +1,32 @@
+#include "object.h"
+#include "car.h"
+#include "sl.h"
+#include "global.h"
+//#include "game.h"
+
+Car::Car(double px,doublepx)
+{
+  posX=px;
+  posY=py;
+}
+
+void Car::loadTexture()
+{
+  texture = slLoadTexture(TEX_CAR);
+ }
+ void Car::draw()
+ {
+  slSprite(texture,posX,posY,50,85);
+ }
+ 
+ void Car::update()
+ {
+  posX=velX;
+  if(posX-23 <=30 || posX+20>=360)
+    velX*=-0.5;
+ }
+ 
+ void Car::control(double cx)
+ {
+  velX+=cx;
+ }
